b082161fcfef2e06f0de8a40cc1c1a4a24548bdd
chris statzer <chris.statzer@gmail.com>
2023-09-29 19:31:41 +0800

    Added sample project

---
+++ OpenTKResizeIssue/Log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Knuckle
+{
+	public class Log
+	{
+		public static void _Log(string level, string msg, ConsoleColor color, params object[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("{0}", DateTime.Now.ToString("HH:mm:ss.ffff"));
+			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.Write(" [");
+			Console.ForegroundColor = color;
+			Console.Write("{0}", level);
+			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.Write("] ");
+			Console.ForegroundColor = ConsoleColor.White;
+			msg = String.Format(msg, args);
+			Console.WriteLine(msg);
+		}
+		public static void Info(string msg, params object[] args)
+        {
+            Knuckle.Log._Log(" INFO", msg, ConsoleColor.Blue, args);
+		}
+
+		public static void Error(string msg, params object[] args)
+		{
+			Knuckle.Log._Log("ERROR", msg, ConsoleColor.Red, args);
+		}
+	}
+}
---
+++ OpenTKResizeIssue/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTKResizeIssue;
+
+public class Window : GameWindow
+{
+
+    public Window(NativeWindowSettings nativeWindowSettings, GameWindowSettings gameWindowSettings) 
+        : base(gameWindowSettings, nativeWindowSettings)
+    {
+        GL.Enable(EnableCap.Blend);
+        GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        SwapBuffers();
+    }
+    protected override void OnKeyDown(KeyboardKeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Keys.F:
+                WindowState = OpenTK.Windowing.Common.WindowState.Fullscreen;
+                break;
+            case Keys.G:
+                WindowState = OpenTK.Windowing.Common.WindowState.Normal;
+                break;
+            default:
+                break;
+        }
+    }
+}
+class MainClass
+{
+
+    public static void Main(string[] args)
+    {
+        var nativeWindowSettings = new NativeWindowSettings()
+        {
+            Size = new Vector2i(1280, 720),
+            Title = "Fullscreen Test",
+            APIVersion = new Version(4, 2),
+        };
+        var gameWindowSettings = GameWindowSettings.Default;
+        var window = new Window(nativeWindowSettings, gameWindowSettings);
+        window.Run();
+    }
+}
